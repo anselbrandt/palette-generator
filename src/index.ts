5040f52386b1c3f94e6d8886b2e6ffcd3a388270
anselbrandt <ansel@anselbrandt.com>
2020-12-28 16:45:00 -0500

    Update palette generator

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import { toArr } from "./jsUtils";
 import { getCustomPalette } from "./paletteUtils";
-import { colors } from "./colors";
+import { toJson } from "./jsUtils";
+import { hexToHsl } from "./colorUtils";
 
 const tomato = "#ff6347";
-const intensityMap = [0.95, 0.9, 0.75, 0.6, 0.3, 0.9, 0.75, 0.6, 0.49];
+// const intensityMap = [0.95, 0.9, 0.75, 0.6, 0.3, 0.9, 0.75, 0.6, 0.49];
 
-const colorsArr = toArr(colors);
-
-console.log(colorsArr);
+const palette = getCustomPalette(tomato);
+const notHex = {
+  ...palette,
+  values: palette.values.map((value) => hexToHsl(value)),
+};
+console.log(notHex);
--- src/paletteUtils.ts
+++ src/paletteUtils.ts
@@ -1,24 +1,17 @@
 import namer from "color-namer";
 
-import {
-  hexToHsl,
-  hslToHex,
-  hexToHsv,
-  hsvToHex,
-  hexToRgb,
-  rgbToHex,
-} from "./colorUtils";
+import { hexToHsl, hslToHex, hexToRgb, rgbToHex } from "./colorUtils";
 
-const changeLightness = (hex: string, intensity: number) => {
+const incrLightness = (hex: string, intensity: number) => {
   let [h, s, l] = hexToHsl(hex);
-  l = l * intensity;
+  l = Math.round(l + (100 - l) * intensity);
   return hslToHex([h, s, l]);
 };
 
-const changeValue = (hex: string, intensity: number) => {
-  let [h, s, v] = hexToHsv(hex);
-  v = v * intensity;
-  return hsvToHex([h, s, v]);
+const decLightness = (hex: string, intensity: number) => {
+  let [h, s, l] = hexToHsl(hex);
+  l = Math.round(l * intensity);
+  return hslToHex([h, s, l]);
 };
 
 const lighten = (hex: string, intensity: number) => {
@@ -38,57 +31,46 @@ const darken = (hex: string, intensity: number) => {
   return rgbToHex([r, g, b]);
 };
 
-interface Palette {
-  [shade: number]: string;
-}
+// export const getPalette = (hex: string) => {
+//   const base = hex;
+//   const name = namer(base).pantone[0].name.toLowerCase().replace(/\s/g, "-");
 
-interface Color {
-  [color: string]: Palette;
-}
+//   const intensityMap: {
+//     [key: number]: number;
+//   } = {
+//     50: 0.95,
+//     100: 0.9,
+//     200: 0.75,
+//     300: 0.6,
+//     400: 0.3,
+//     600: 0.9,
+//     700: 0.75,
+//     800: 0.6,
+//     900: 0.49,
+//   };
 
-export const getPalette = (hex: string) => {
-  const name = namer(hex).pantone[0].name.toLowerCase().replace(/\s/g, "-");
-  const color: Color = {
-    [name]: {
-      500: `#${hex.replace(/#/g, "")}`,
-    },
-  };
-  const intensityMap: {
-    [key: number]: number;
-  } = {
-    50: 0.95,
-    100: 0.9,
-    200: 0.75,
-    300: 0.6,
-    400: 0.3,
-    600: 0.9,
-    700: 0.75,
-    800: 0.6,
-    900: 0.49,
-  };
+//   const lighter = [50, 100, 200, 300, 400].map((level) =>
+//     lighten(base, intensityMap[level])
+//   );
+//   const darker = [600, 700, 800, 900].map((level) =>
+//     darken(base, intensityMap[level])
+//   );
 
-  [50, 100, 200, 300, 400].forEach((level) => {
-    color[name][level] = lighten(hex, intensityMap[level]);
-  });
-  [600, 700, 800, 900].forEach((level) => {
-    color[name][level] = darken(hex, intensityMap[level]);
-  });
-
-  return color;
-};
+//   return {
+//     name: name,
+//     base: base,
+//     values: [...lighter, base, ...darker],
+//   };
+// };
 
 interface Options {
   colorSpace?: string;
   intensityMap?: number[];
 }
 
-export const getCustomPalette = (hex: string, options?: Options) => {
-  const name = namer(hex).pantone[0].name.toLowerCase().replace(/\s/g, "-");
-  const color: Color = {
-    [name]: {
-      500: `#${hex.replace(/#/g, "")}`,
-    },
-  };
+export const getPalette = (hex: string, options?: Options) => {
+  const base = hex;
+  const name = namer(base).pantone[0].name.toLowerCase().replace(/\s/g, "-");
 
   let intensityMap: {
     [key: number]: number;
@@ -102,6 +84,7 @@ export const getCustomPalette = (hex: string, options?: Options) => {
     200: 0.75,
     300: 0.6,
     400: 0.3,
+    500: 1,
     600: 0.9,
     700: 0.75,
     800: 0.6,
@@ -109,7 +92,7 @@ export const getCustomPalette = (hex: string, options?: Options) => {
   };
 
   if (options?.intensityMap && options.intensityMap.length === 9) {
-    const mapKeys = [50, 100, 200, 300, 400, 600, 700, 800, 900];
+    const mapKeys = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
     const values = options.intensityMap;
     const entries = mapKeys.map((key, index) => [key, values[index]]);
     const customMap = Object.fromEntries(entries);
@@ -118,25 +101,29 @@ export const getCustomPalette = (hex: string, options?: Options) => {
     intensityMap = defaultMap;
   }
 
+  let lighter;
+  let darker;
   switch (options?.colorSpace) {
     case "hsl":
-      [50, 100, 200, 300, 400, 600, 700, 800, 900].forEach((level) => {
-        color[name][level] = changeLightness(hex, intensityMap[level]);
-      });
-      break;
-    case "hsv":
-      [50, 100, 200, 300, 400, 600, 700, 800, 900].forEach((level) => {
-        color[name][level] = changeValue(hex, intensityMap[level]);
-      });
+      lighter = [50, 100, 200, 300, 400].map((level) =>
+        incrLightness(base, intensityMap[level])
+      );
+      darker = [600, 700, 800, 900].map((level) =>
+        decLightness(base, intensityMap[level])
+      );
       break;
     default:
-      [50, 100, 200, 300, 400].forEach((level) => {
-        color[name][level] = lighten(hex, intensityMap[level]);
-      });
-      [600, 700, 800, 900].forEach((level) => {
-        color[name][level] = darken(hex, intensityMap[level]);
-      });
+      lighter = [50, 100, 200, 300, 400].map((level) =>
+        lighten(base, intensityMap[level])
+      );
+      darker = [600, 700, 800, 900].map((level) =>
+        darken(base, intensityMap[level])
+      );
   }
 
-  return color;
+  return {
+    name: name,
+    base: base,
+    values: [...lighter, base, ...darker],
+  };
 };
